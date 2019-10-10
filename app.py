5cceb0c53213e007d99543d8c69ff2c727637bbd
Tushar Sharma <tushar.sharma118@gmail.com>
2019-10-10 14:45:00 +0530

    code refactor

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
+
 import tensorflow as tf
 import skimage
 import math
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+
 model2 = tf.keras.models.load_model('final_model.h5')
 ## Begin
 
@@ -135,19 +135,6 @@ def upload_file():
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
 if __name__ == "__main__":
